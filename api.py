4f1781928f0de28bb6d7f1bb3a70baee941bd44a
ouzezhou <ouzezhou@JT103068.staff.neworiental.org>
2019-07-19 11:27:55 +0800

    update

--- api.py
+++ api.py
@@ -15,18 +15,32 @@ class HelloWorld(Resource):
 
 
 class WarUpdate(Resource):
-	def post(self):
-		try:
-			fileUpload = request.files.get('fileUpload')
-			if not fileUpload:
-				abort(500, "fileUpload not exists")
-			fileUpload.save('swap.war')
-			os.system('sh -ex updateWar.sh')
-			return {'status':1}
-		except Exception as e:
-			abort(500, str(e))
-		finally:
-			os.system('rm -rf swap.war')
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('tomcatPath')
+            parser.add_argument('fileName')
+            args = parser.parse_args()
+
+            tomcatPath = args.get('tomcatPath')
+            if not tomcatPath:
+                abort(500, "tomcatPath is null")
+            fileName = args.get('fileName')
+            if not fileName:
+                abort(500, "fileName is null")
+            fileUpload = request.files.get('fileUpload')
+            if not fileUpload:
+                abort(500, "fileUpload not exists")
+
+            os.system('mkdir -p upload')
+            fileUpload.save(f'upload/{fileName}')
+
+            os.system(f'sh -ex updateWar.sh {tomcatPath} {fileName}')
+            return {'status':1}
+        except Exception as e:
+            abort(500, str(e))
+        finally:
+            os.system('rm -rf swap.war')
 
 
 api.add_resource(HelloWorld, '/')
@@ -35,31 +49,34 @@ api.add_resource(WarUpdate, '/update')
 
 @app.before_request
 def auth():
-	if(request.path.startswith('/error')):
-		return
-	parser = reqparse.RequestParser()
-	#parser.add_argument('token')
-	parser.add_argument('token', location='headers')
-	args = parser.parse_args()
-	token = args.get('token')
-	if token:
-		secretKey = 'UuQjLlru4SOuGGLKWn1cGH0YeBZQlFfD'
-		content = secretKey+datetime.datetime.now().strftime("%Y-%m-%d_%H")
-		m2 = hashlib.md5()
-		m2.update(content.encode("UTF-8"))
-		if m2.hexdigest() == token:
-			return
-		content = secretKey+(datetime.datetime.now()+datetime.timedelta(hours=-1)).strftime("%Y-%m-%d_%H")
-		m2 = hashlib.md5()
-		m2.update(content.encode("UTF-8"))
-		if m2.hexdigest() == token:
-			return
-		content = secretKey+(datetime.datetime.now()+datetime.timedelta(hours=1)).strftime("%Y-%m-%d_%H")
-		m2 = hashlib.md5()
-		m2.update(content.encode("UTF-8"))
-		if m2.hexdigest() == token:
-			return
-	return abort(403, "FORBIDDEN")
+    if(request.path.startswith('/error')):
+        return
+    parser = reqparse.RequestParser()
+    parser.add_argument('token', location='headers')
+    args = parser.parse_args()
+
+    token = args.get('token')
+    if token:
+        secretKey = 'UuQjLlru4SOuGGLKWn1cGH0YeBZQlFfD'
+
+        content = secretKey+datetime.datetime.now().strftime("%Y-%m-%d_%H")
+        m2 = hashlib.md5()
+        m2.update(content.encode("UTF-8"))
+        if m2.hexdigest() == token:
+            return
+
+        content = secretKey+(datetime.datetime.now()+datetime.timedelta(hours=-1)).strftime("%Y-%m-%d_%H")
+        m2 = hashlib.md5()
+        m2.update(content.encode("UTF-8"))
+        if m2.hexdigest() == token:
+            return
+
+        content = secretKey+(datetime.datetime.now()+datetime.timedelta(hours=1)).strftime("%Y-%m-%d_%H")
+        m2 = hashlib.md5()
+        m2.update(content.encode("UTF-8"))
+        if m2.hexdigest() == token:
+            return
+    return abort(403, "FORBIDDEN")
 
 
 if __name__ == '__main__':
--- updateWar.sh
+++ updateWar.sh
@@ -1,19 +1,49 @@
 #!/usr/bin/env sh
-tp={tomcat_path}
-context={web_context}
+tomcatPath=$1
+fileName=$2
+
+if [ -z "${tomcatPath}" ]
+then
+  echo "tomcatPath is null"
+  exit 1
+fi
+if [ -z "${fileName}" ]
+then
+  echo "fileName is null"
+  exit 1
+fi
+
+context=$(echo "${fileName}" | sed s/\\..*//)
+fileType=$(echo "${fileName}" | sed s/.*\\.//)
+
+if [ -z "${context}" ]
+then
+  echo "context is null"
+  exit 1
+fi
+
 wp=$(pwd)
+cd ${tomcatPath}
 
-cd $tp
-
-ps -ef | grep -v "grep " | grep "$tp"
-if [ $? -eq 0 ]
+ps -ef | grep "${tomcatPath}/" | grep -v "grep " && res=$? || res=$?
+if [ ${res} -eq 0 ]
 then
   sh bin/shutdown.sh
   sleep 10s
 fi
 
-rm -rf webapps/${context}.war webapps/${context}
-cp $wp/swap.war webapps/${context}.war
+rm -rf webapps/${context}
+if [ "${fileType}" = "war" ]
+then
+  unzip -o $wp/upload/${fileName} -d webapps/${context}
+elif [ "${fileType}" = "tgz" ]
+then
+  mkdir -p webapps/${context}
+  tar -xf $wp/upload/${fileName} -C webapps/${context}
+else
+  echo "fileType not support: ${fileType}"
+  exit 1
+fi
 
 sh bin/startup.sh
-ps -ef | grep -v "grep " | grep "$tp"
+ps -ef | grep "${tomcatPath}/" | grep -v "grep "
